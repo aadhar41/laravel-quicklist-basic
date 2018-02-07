ab9dedebea1435c4bb67b27b57b6260415e54e6f
Aadhar gaur <aadhargaur@planetwebsolution.com>
2018-02-07 12:45:14 +0530

    Laravel quick tasklist starts

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+/**
+ * Display All Tasks
+ */
+Route::get('/', function () {
+    return view('tasks');
+});
+
+
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
+});
