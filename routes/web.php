d63ab567dfb5b7905004e89c2c5864943a2c2cef
Aadhar gaur <aadhargaur@planetwebsolution.com>
2018-02-07 13:28:36 +0530

    added errors common template, added validation handler in routes.

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,23 @@ Route::get('/', function () {
  * Add A New Task
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+
+    // Create The Task...
 });
 
 
